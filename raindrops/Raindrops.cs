5ea23b92728059b6e4bc8b79ee49e2b47669b318
Kristofer Hoadley <kristoferhoadley@gmail.com>
2018-06-06 16:28:43 -0700

    Added new challenges
    -Raindrops is complete
    -Sum of Multiples is complete
    -Two-Fer is new

---
+++ raindrops/Raindrops.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public static class Raindrops
+{
+    private static Dictionary<int, string> map = new Dictionary<int, string>
+    {
+        {3, "Pling"},
+        {5, "Plang"},
+        {7, "Plong"}
+    };
+
+    public static string Convert(int number)
+    {
+        var result = new StringBuilder();
+
+        foreach(int key in map.Keys)
+        {
+            if (number % key == 0)
+            {
+                result.Append(map[key]);
+            }
+        }
+        if(result.Length == 0)
+        {
+            result.Append(number.ToString());
+        }
+
+        return result.ToString();
+    }
+}
--- sum-of-multiples/SumOfMultiples.cs
+++ sum-of-multiples/SumOfMultiples.cs
@@ -5,6 +5,30 @@ public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return SumHelper(multiples, max - 1);
+    }
+
+    private static int SumHelper(IEnumerable<int> multiples, int current)
+    {
+        if(current < 1)
+        {
+            return 0;
+        }
+        else
+        {
+            return current.IsMultipleOf(multiples) ? current + SumHelper(multiples, current - 1) : SumHelper(multiples, current - 1);
+        }
+    }
+
+    private static Boolean IsMultipleOf(this int numerator, IEnumerable<int> denominators)
+    {
+        foreach (int denominator in denominators)
+        {
+            if (numerator % denominator == 0)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
