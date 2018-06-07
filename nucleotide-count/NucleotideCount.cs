436d5a58efdb7cfb93c248259bb2cc9218b61d7b
Kristofer Hoadley <kristoferhoadley@gmail.com>
2018-06-07 16:09:12 -0700

    NucleotideCount initial solution

---
+++ nucleotide-count/NucleotideCount.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class NucleotideCount
+{
+    private Dictionary<char, int> nucleotideCounts = new Dictionary<char, int>
+    {
+        {'A', 0},
+        {'C', 0},
+        {'G', 0},
+        {'T', 0}
+    };
+    public NucleotideCount(string sequence)
+    {
+        foreach (var letter in sequence)
+        {
+            if(nucleotideCounts.ContainsKey(letter))
+            {
+                nucleotideCounts[letter] = nucleotideCounts[letter] + 1;
+            }
+            else
+            {
+                throw new InvalidNucleotideException();
+            }
+        }
+    }
+
+    public IDictionary<char, int> NucleotideCounts
+    {
+        get
+        {
+            return nucleotideCounts;
+        }
+    }
+    
+}
+
+public class InvalidNucleotideException : Exception { }
