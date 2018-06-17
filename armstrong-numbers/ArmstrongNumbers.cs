9928d2d61c88df22d61d8d22cf4d3339ba768c87
Kristofer Hoadley <kristoferhoadley@gmail.com>
2018-06-17 16:47:03 -0700

    Armstrong-Numbers initial solution using expression bodies

--- armstrong-numbers/ArmstrongNumbers.cs
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -2,8 +2,21 @@
 
 public static class ArmstrongNumbers
 {
-    public static bool IsArmstrongNumber(int number)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static bool IsArmstrongNumber(int number) =>
+        CalculateArmstrong(number.ToString(), number.ToString().Length) == number;
+
+    public static int CalculateArmstrong(string intAsString, int power) =>
+        intAsString.Length == 1 ?
+        int.Parse(intAsString).Pow(power) :
+        CalculateArmstrong(intAsString.Substring(1), power) + int.Parse(intAsString.Substring(0, 1)).Pow(power);
+
+    public static int Pow(this int number, int power) =>
+        power == 0 ?
+        1 :
+        (
+            power == 1 ?
+            number :
+            number * number.Pow(power - 1)
+        );
+
 }
