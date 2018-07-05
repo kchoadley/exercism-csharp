7091fb11b578b8fffd4de268e1dd40651d1d3193
Kristofer Hoadley <kristoferhoadley@gmail.com>
2018-07-05 16:23:35 -0700

    KindergartenGarden added

---
+++ kindergarten-garden/KindergartenGarden.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum Plant
+{
+    Violets,
+    Radishes,
+    Clover,
+    Grass
+}
+
+public class KindergartenGarden
+{
+    static List<string> children = new List<string>()
+    {
+        "Alice", "Bob", "Charlie", "David",
+        "Eve", "Fred", "Ginny", "Harriet",
+        "Ileana", "Joseph", "Kincaid", "Larry"
+    };
+    Dictionary<String, List<Plant>> kindergartenersPlants;
+    public KindergartenGarden(string diagram) : this(diagram, children)
+    {
+    }
+
+    public KindergartenGarden(string diagram, IEnumerable<string> students)
+    {
+        var rows = diagram.Split("\n");
+    }
+
+    public IEnumerable<Plant> Plants(string student)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
