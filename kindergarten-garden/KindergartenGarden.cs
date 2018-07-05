4ef05af97ebaca474f145c22d7f01fdf75c32535
Kristofer Hoadley <kristoferhoadley@gmail.com>
2018-07-05 16:57:02 -0700

    KidergartenGarden Partially tested solution

--- kindergarten-garden/KindergartenGarden.cs
+++ kindergarten-garden/KindergartenGarden.cs
@@ -11,24 +11,50 @@ public enum Plant
 
 public class KindergartenGarden
 {
-    static List<string> children = new List<string>()
+    static string[] children = new List<string>()
     {
         "Alice", "Bob", "Charlie", "David",
         "Eve", "Fred", "Ginny", "Harriet",
         "Ileana", "Joseph", "Kincaid", "Larry"
+    }.ToArray();
+    Dictionary<String, List<Plant>> kindergartenersPlants = new Dictionary<string, List<Plant>>();
+    Dictionary<char, Plant> mapPlants = new Dictionary<char, Plant>
+    {
+        {  'V', Plant.Violets },
+        {  'R', Plant.Radishes },
+        {  'C', Plant.Clover },
+        {  'G', Plant.Grass }
     };
-    Dictionary<String, List<Plant>> kindergartenersPlants;
     public KindergartenGarden(string diagram) : this(diagram, children)
     {
     }
 
     public KindergartenGarden(string diagram, IEnumerable<string> students)
     {
+        foreach(var child in children)
+        {
+            kindergartenersPlants.Add(child, new List<Plant>());
+        }
+
         var rows = diagram.Split("\n");
+
+        foreach (var row in rows)
+        {
+            for (int count = 0; count < rows[0].Length; count++)
+            {
+                int child = count / 2;
+
+                var character = row[count];
+                var plant = mapPlants[character];
+                kindergartenersPlants[children[child]].Add(plant);
+
+                //kindergartenersPlants[children[child]].Add(mapPlants[row[count]]);
+            }
+        }
     }
 
     public IEnumerable<Plant> Plants(string student)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return kindergartenersPlants[student].ToArray();
     }
 }
