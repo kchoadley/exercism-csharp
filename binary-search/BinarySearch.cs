130cd97a48b87349199ee146a665f17c2b51c131
Kristofer Hoadley <kristoferhoadley@gmail.com>
2018-07-06 11:05:00 -0700

    BinarySearch Initial solution, recursive

--- binary-search/BinarySearch.cs
+++ binary-search/BinarySearch.cs
@@ -2,13 +2,37 @@
 
 public class BinarySearch
 {
+    private readonly int[] array;
     public BinarySearch(int[] input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        array = input;
     }
 
-    public int Find(int value)
-    {
-        throw new NotImplementedException("You need to implement this function.");
+    public int Find(int value) => array.Length == 0?
+        -1:
+        FindHelper(0, array.Length - 1, value);
+
+    private int FindHelper(int min, int max, int value) {
+        int result = -1;
+        int index = (max + min + 1) / 2;
+        if (array[index] == value)
+        {
+            result = index;
+        }
+        else if (value < array[index])
+        {
+            if (index != 0)
+            {
+                result = FindHelper(0, index - 1, value);
+            }
+        }
+        else
+        {
+            if (index != max)
+            {
+                result = FindHelper(index + 1, max, value);
+            }
+        }
+        return result;
     }
 }
