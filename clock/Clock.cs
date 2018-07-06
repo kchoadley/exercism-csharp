1582d08656b62e1fffb28ad231459103dece496e
Kristofer Hoadley <kristoferhoadley@gmail.com>
2018-07-06 10:16:44 -0700

    Clock More concise solution

--- clock/Clock.cs
+++ clock/Clock.cs
@@ -2,43 +2,27 @@ using System;
 
 public struct Clock
 {
-    const int MinutesPerHour = 60;
-    const int MinutesPerDay = 60 * 24;
+    private const int MinutesPerHour = 60;
+    private const int MinutesPerDay = 60 * 24;
+    private readonly int timeOfDayInMinutes;
 
-    private int minutes;
+    public readonly int Hours;
+    public readonly int Minutes;
+
     public Clock(int hours, int minutes)
     {
-        this.minutes = (MinutesPerDay + ((hours * MinutesPerHour + minutes) % (MinutesPerDay))) % (MinutesPerDay);
+        this.timeOfDayInMinutes = (MinutesPerDay + ((hours * MinutesPerHour + minutes) % (MinutesPerDay))) % (MinutesPerDay);
+        this.Hours = this.timeOfDayInMinutes / MinutesPerHour;
+        this.Minutes = this.timeOfDayInMinutes % MinutesPerHour;
     }
 
-    public int Hours
-    {
-        get
-        {
-            return this.minutes / MinutesPerHour;
-        }
-    }
 
-    public int Minutes
-    {
-        get
-        {
-            return this.minutes % MinutesPerHour;
-        }
-    }
+    public Clock Add(int minutesToAdd) => new Clock(0, this.timeOfDayInMinutes + minutesToAdd);
 
-    public Clock Add(int minutesToAdd)
-    {
-        return new Clock(0, this.minutes + minutesToAdd);
-    }
-
-    public Clock Subtract(int minutesToSubtract)
-    {
-        return new Clock(0, this.minutes - minutesToSubtract);
-    }
+    public Clock Subtract(int minutesToSubtract) => new Clock(0, this.timeOfDayInMinutes - minutesToSubtract);
 
     public override string ToString()
     {
-        return $"{Hours,0:D2}:{Minutes,0:D2}";
+        return $"{Hours:D2}:{Minutes:D2}";
     }
 }
